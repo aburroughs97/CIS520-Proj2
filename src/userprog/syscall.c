d33749dd469adcce7df5067cc8c9dab8f539ab57
Alexander Burroughs <aburroughs97@cougar.cs.ksu.edu>
2019-03-01 18:11:52 -0600

    Added exit and write

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,11 +1,26 @@
 #include "userprog/syscall.h"
-#include <syscall.h>
 #include <stdio.h>
+#include "lib/kernel/stdio.h"
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+
+//System call functions
+void halt (void) NO_RETURN;
+void exit (int status) NO_RETURN;
+pid_t exec (const char *file);
+int wait (pid_t);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned length);
+int write (int fd, const void *buffer, unsigned length);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
 
 void
 syscall_init (void) 
@@ -78,7 +93,7 @@ halt (void)
 void 
 exit (int status)
 {
-
+  thread_exit();
 }
 
 pid_t 
@@ -130,6 +145,7 @@ write (int fd, const void *buffer, unsigned size)
   if(fd == 1)
   {
     putbuf(buffer, size);
+    return size;
   }
 }
 
